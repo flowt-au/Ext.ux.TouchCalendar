d634de54f70129b56f64d404292843ad8d4a2fca
ksincennes <kevin.sincennes@farmersedge.ca>
2016-01-14 22:10:30 +0000

    Fixed how events over multiple days are handled by event bar height calculation

--- Ext.ux.TouchCalendarEvents/Ext.ux.TouchCalendarDayEvents.js
+++ Ext.ux.TouchCalendarEvents/Ext.ux.TouchCalendarDayEvents.js
@@ -3,7 +3,7 @@
  */
 Ext.define('Ext.ux.TouchCalendarDayEvents', {
 
-    extend: 'Ext.ux.TouchCalendarEventsBase',
+	extend: 'Ext.ux.TouchCalendarEventsBase',
 
 	config: {
 		/**
@@ -12,30 +12,49 @@ Ext.define('Ext.ux.TouchCalendarDayEvents', {
 		eventSortDirection: 'ASC'
 	},
 
-	eventFilterFn: function(record, id, currentDateTime){
-		var startDate   = this.getRoundedTime(record.get(this.getPlugin().getStartEventField())).getTime(),
-			endDate     = this.getRoundedTime(record.get(this.getPlugin().getEndEventField())).getTime();
+	eventFilterFn: function(record, id, currentDateTime) {
+		var startDate = this.getRoundedTime(record.get(this.getPlugin().getStartEventField())).getTime(),
+			endDate = this.getRoundedTime(record.get(this.getPlugin().getEndEventField())).getTime();
 
 		return (startDate <= currentDateTime) && (endDate >= currentDateTime);
 	},
 
-	renderEventBars: function(store){
+	renderEventBars: function(store) {
 		var me = this,
 			l = store.getCount(),
 			i = 0,
 			rec;
 
-		for(; i < l; i++){
+		for (; i < l; i++) {
 			rec = store.getAt(i);
 
-			var eventRecord     = rec.data.Record,
-				eventBar        = this.createEventBar(rec, eventRecord),
+			var eventRecord = rec.data.Record;
 
-				eventWidth      = this.getEventBarWidth(rec, 50+10), // 50 = left margin, 10 = right margin TODO: make configurable
+			var doesWrap = this.eventBarDoesWrap(rec),
+				hasWrapped = this.eventBarHasWrapped(rec),
+				cssClasses = [
+					this.getPlugin().getEventBarCls(),
+					'e-' + rec.get('EventID'), (doesWrap ? ' wrap-end' : ''), (hasWrapped ? ' wrap-start' : ''),
+					eventRecord.get(this.getPlugin().getCssClassField())
+				];
 
-				verticalPos     = this.getVerticalDayPosition(rec),
-				horizontalPos   = this.getHorizontalDayPosition(rec, eventWidth),
-				eventHeight     = this.getEventBarHeight(rec);
+
+			// create the event bar
+			var eventBar = Ext.DomHelper.append(this.getPlugin().getEventWrapperEl(), {
+				tag: 'div',
+				style: {
+					'background-color': eventRecord.get(this.getPlugin().colourField)
+				},
+				html: this.getPlugin().getEventBarTpl().apply(eventRecord.data),
+				eventID: rec.get('EventID'),
+				cls: cssClasses.join(' ')
+			}, true);
+
+			var eventWidth = this.getEventBarWidth(rec, 50 + 10), // 50 = left margin, 10 = right margin TODO: make configurable
+
+				verticalPos = this.getVerticalDayPosition(rec),
+				horizontalPos = this.getHorizontalDayPosition(rec, eventWidth),
+				eventHeight = this.getEventBarHeight(rec);
 
 			eventBar.setLeft(horizontalPos);
 			eventBar.setTop(verticalPos - this.getCalendar().element.getY());
@@ -46,60 +65,66 @@ Ext.define('Ext.ux.TouchCalendarDayEvents', {
 
 	},
 
-	getEventBarWidth: function(event, offset){
-		var eventsInTimeSlot    = this.getEventsPerTimeSlot()[event.get('Date').getTime()],
-			calendarWidth       = this.getCalendar().element.getWidth();
+	getEventBarWidth: function(event, offset) {
+		var eventsInTimeSlot = this.getEventsPerTimeSlot()[event.get('Date').getTime()],
+			calendarWidth = this.getCalendar().element.getWidth();
 
-		eventsInTimeSlot    = eventsInTimeSlot || 1;
-		offset              = offset || 0;
+		eventsInTimeSlot = eventsInTimeSlot || 1;
+		offset = offset || 0;
 
 		return Math.floor((calendarWidth - offset) / eventsInTimeSlot);
 	},
 
-	getEventBarHeight: function(event){
+	getEventBarHeight: function(event) {
 		var eventHeight = this.getPlugin().getEventHeight();
 
-		if(Ext.isNumeric(eventHeight)){
+		if (Ext.isNumeric(eventHeight)) {
 			return eventHeight;
-		} else if(eventHeight === 'duration'){
+		} else if (eventHeight === 'duration') {
 			return this.getEventBarHeightDuration(event);
 		} else {
 			return 'auto';
 		}
 	},
 
-	getEventBarHeightDuration: function(event){
-		var startDate           = event.data.Record.get(this.getPlugin().getStartEventField()),
-			endDate             = event.data.Record.get(this.getPlugin().getEndEventField()),
-			roundedStartDate    = this.getRoundedTime(startDate),
-			minutesLength       = (endDate.getTime() - startDate.getTime()) / 1000 / 60,
-			timeSlotEl          = this.getCalendar().getDateCell(roundedStartDate),
-			timeSlotRowEl       = timeSlotEl.parent('tr', false),
-			heightPixels        = 0;
+	getEventBarHeightDuration: function(event) {
+		var startDate = event.data.Record.get(this.getPlugin().getStartEventField()),
+			endDate = event.data.Record.get(this.getPlugin().getEndEventField()),
+			roundedStartDate = this.getRoundedTime(startDate),
+			minutesLength = (endDate.getTime() - startDate.getTime()) / 1000 / 60;
+		if (roundedStartDate < this.getCalendar().currentDate)
+			roundedStartDate = this.getRoundedTime(this.getCalendar().currentDate)
+		var timeSlotEl = this.getCalendar().getDateCell(roundedStartDate),
+			timeSlotRowEl = timeSlotEl.parent('tr', false),
+			heightPixels = 0;
 
-		if(timeSlotRowEl){
-			var timeSlotHeight  = timeSlotEl.getHeight(),
+		if (timeSlotRowEl) {
+			var timeSlotHeight = timeSlotEl.getHeight(),
 				minutesPerPixel = timeSlotHeight / 30;
 
-			heightPixels    = minutesLength * minutesPerPixel;
+			heightPixels = minutesLength * minutesPerPixel;
 		}
 
 		return heightPixels;
 	},
 
-	getVerticalDayPosition: function(event){
-		var startDate           = event.data.Record.get(this.getPlugin().getStartEventField()),
-			roundedStartDate    = this.getRoundedTime(startDate),
-			timeSlotCount       = (roundedStartDate.getHours() * 2) + (roundedStartDate.getMinutes() === 30 ? 1 : 0),
-			minutesDiff         = (startDate.getTime() - roundedStartDate.getTime()) / 1000 / 60,
-			firstTimeSlotEl     = this.getCalendar().element.select('table.time-slot-table td', this.getCalendar().element.dom).first(),
-			verticalPosition    = 0;
+	getVerticalDayPosition: function(event) {
+		var startDate = event.data.Record.get(this.getPlugin().getStartEventField()),
+			roundedStartDate = this.getRoundedTime(startDate),
+			timeSlotCount = (roundedStartDate.getHours() * 2) + (roundedStartDate.getMinutes() === 30 ? 1 : 0),
+			minutesDiff = (startDate.getTime() - roundedStartDate.getTime()) / 1000 / 60,
+			firstTimeSlotEl = this.getCalendar().element.select('table.time-slot-table td', this.getCalendar().element.dom).first(),
+			verticalPosition = 0;
+		if (roundedStartDate < this.getCalendar().currentDate) {
+			timeSlotCount = 0;
+			minutesDiff = 0;
+		}
 
-		if(firstTimeSlotEl){
+		if (firstTimeSlotEl) {
 			var firstTimeSlotHeight = firstTimeSlotEl.getHeight(),
-				firstTimeSlotY      = firstTimeSlotEl.getY(), // first time slot position - needed so we take the header row into account
-				minutesPerPixel     = firstTimeSlotHeight / 30,
-				extraMinutesY       = minutesDiff * minutesPerPixel;
+				firstTimeSlotY = firstTimeSlotEl.getY(), // first time slot position - needed so we take the header row into account
+				minutesPerPixel = firstTimeSlotHeight / 30,
+				extraMinutesY = minutesDiff * minutesPerPixel;
 
 			verticalPosition = firstTimeSlotY + (timeSlotCount * firstTimeSlotHeight) + extraMinutesY;
 		}
@@ -107,9 +132,9 @@ Ext.define('Ext.ux.TouchCalendarDayEvents', {
 		return verticalPosition;
 	},
 
-	getHorizontalDayPosition: function(event, eventBarWidth){
-		var barPos      = event.get('BarPosition'),
-			leftMargin  = 50,
+	getHorizontalDayPosition: function(event, eventBarWidth) {
+		var barPos = event.get('BarPosition'),
+			leftMargin = 50,
 			spacing = this.getPlugin().getEventBarSpacing();
 
 		return leftMargin + (barPos * eventBarWidth) + (barPos * spacing);
@@ -122,7 +147,7 @@ Ext.define('Ext.ux.TouchCalendarDayEvents', {
 	 * @param {Date} date
 	 * @return {Date}
 	 */
-	getRoundedTime: function(date){
+	getRoundedTime: function(date) {
 		date = Ext.Date.clone(date);
 
 		var minutes = date.getMinutes();
